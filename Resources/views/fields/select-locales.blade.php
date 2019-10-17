b51df5ba09e2d47d777fdbeb1bbab0169be6daa7
Pushpendra Chouhan <pushpendra.c@cisinlabs.com>
2019-10-17 16:03:31 +0530

    PSCMS Core Module Intial Commit

---
+++ Resources/views/fields/select-locales.blade.php
@@ -0,0 +1,21 @@
+<div class="form-group">
+    <label for="{{ $settingName }}">{{ trans($moduleInfo['description']) }}</label>
+    <select multiple class="locales" name="{{ $settingName }}[]" id="{{ $settingName }}">
+        @foreach ($locales as $id => $locale)
+        <option value="{{ $id }}" {{ isset($dbSettings[$settingName]) && isset(array_flip(json_decode($dbSettings[$settingName]->plainValue))[$id]) ? 'selected' : '' }}>
+            {{ array_get($locale, 'name') }}
+        </option>
+        @endforeach
+    </select>
+</div>
+@section('scripts')
+<script>
+    $( document ).ready(function() {
+        $('.locales').selectize({
+            delimiter: ',',
+            plugins: ['remove_button']
+        });
+    });
+</script>
+@stop
+
